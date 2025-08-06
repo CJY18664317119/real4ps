a31398fa18a23f8a0323f025a59b108ea314c82d
CJY18664317119 <166104656+CJY18664317119@users.noreply.github.com>
2025-08-06 15:43:53 +0800

    添加404页面

---
+++ 前端/src/pages/NotFound.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="not-found">
+    <h1>404 - 页面未找到</h1>
+    <p>您访问的页面不存在或已被移除</p>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  padding: 50px;
+}
+
+h1 {
+  font-size: 2em;
+  color: #ff4444;
+}
+
+p {
+  font-size: 1.2em;
+  color: #666;
+}
+</style>
